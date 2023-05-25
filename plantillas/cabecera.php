b89f2247db6498a25e152e629f10f5edcabf7a33
NoeRuiTor <noeruitor@alu.edu.gva.es>
2023-05-26 01:06:03 +0200

    solucionar problema con scrip navegador responsive

--- plantillas/cabecera.php
+++ plantillas/cabecera.php
@@ -41,24 +41,23 @@
       
 <script>
 const limit_size_screen = window.matchMedia('screen and (max-width: 768px)');
-const bars = document.querySelector('#bars');
-const barsButton = document.querySelector('.menu-icon');
+const menu = document.querySelector('#bars');
+const menuIcon = document.querySelector('.menu-icon');
 
 function validation(event) {
-	if (event.matches) {
-		barsButton.addEventListener('click', hideShow);
-	} else {
-		barsButton.removeEventListener('click', hideShow);
-	}
+  if (event.matches) {
+    menuIcon.addEventListener('click', hideShow);
+  } else {
+    menuIcon.removeEventListener('click', hideShow);
+    menu.classList.remove('is-active'); // Asegurarse de ocultar el menú en pantallas grandes
+  }
 }
+
 validation(limit_size_screen);
 
 function hideShow() {
-	if (bars.classList.contains('is-active')) {
-		bars.classList.remove('is-active');
-	} else {
-		bars.classList.add('is-active');
-	}
+  menu.classList.toggle('is-active'); // Agrega o quita la clase 'is-active' para mostrar u ocultar el menú
 }
+
 </script>
     
