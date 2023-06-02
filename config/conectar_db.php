790d76341a99644bedacdb06e9ee9c1647d7c8d1
NoeRuiTor <noeruitor@alu.edu.gva.es>
2023-06-02 10:59:39 +0200

    configuraciones para conexión base de datos

--- config/conectar_db.php
+++ config/conectar_db.php
@@ -11,7 +11,7 @@ function conectar_db($bd) {
     try {
         
         $username = 'fundeanu'; // Reemplaza 'root' con tu usuario de la base de datos
-        $password = 'password123456'; // Reemplaza '' con tu contraseña de la base de datos
+        $password = 'password1234'; // Reemplaza '' con tu contraseña de la base de datos
         $dbName = $bd; // Nombre de la base de datos
         $instanceUnixSocket = '/cloudsql/balmy-doodad-386421:europe-west1:bd-electricidadfcv'; // Ruta del socket UNIX de Cloud SQL
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");  
