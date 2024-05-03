35a69b4106d2c1b39600c006623953cf8c7ba984
NoeRuiTor <noeruitor@alu.edu.gva.es>
2024-05-03 21:07:32 +0200

    añadir validación formulario de contacto en el lado del servidor

--- public/contacto_Controller.php
+++ public/contacto_Controller.php
@@ -3,8 +3,9 @@ function procesarFormContacto(){
     $nombre = filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);
     $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_NUMBER_INT);
     $email = filter_var ($_POST['email'],FILTER_SANITIZE_EMAIL);
+    $pais = $_POST['pais'];
     $consulta = $_POST['consulta'];
-	    
+    if (validarDatos($nombre, $email, $telefono, $pais) == true){    
 
         $to ='fundeanuconstantin@gmail.com'; // Dirección de correo electrónico a la que se enviará el mensaje
         $subject = 'Contacto desde la web'; // Asunto del correo electrónico
@@ -29,9 +30,33 @@ function procesarFormContacto(){
             header("location:contacto.php?error=$error");
             exit();
         }
+    }else{
+        $error='Datos inválidos';
+        header("location:contacto.php?error=$error");
+        exit(); 
+    }
     
     
+}
+
+function validarDatos($nombre, $email,$telefono,$pais){
+  
+    if (!isset($nombre) || empty($nombre)){
+        return false;
+    }
+    if (!isset($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        return false;
+    }
     
+    if (!isset($telefono) || empty($telefono)){
+        return false;
+    }
+    if (!empty($pais)){
+       return false;
+    }
+   
+  
+    return true;
 }
 
 // Llamar al controlador para procesar el formulario
